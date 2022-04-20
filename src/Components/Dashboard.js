6d51d34b794059def32554888c2ea14c107e2bed
paktolus-mohit <msahu@paktolus.com>
2022-04-20 17:49:25 +0530

    POST method update and PUT

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+// import validator from 'validator';
 // import { useEffect } from "react";
 import Swal from "sweetalert2";
 const Dashboard = () => {
+  const [user, setUser] = useState({
+    First_name: "",
+    Last_name: "",
+    Email: "",
+    Phone_no: "",
+    Dob: "",
+    Gender: "",
+  });
+  const [updateUser, setUpdateUser]=useState({
+    First_name: "",
+    Last_name: "",
+    Email: "",
+    Phone_no: "",
+    Dob: "",
+    Gender: "",
+  })
+  const [btn, setBtn] = useState(true);
+  const [borderColor, setBorderColor] = useState("1px solid blue");
+
   function deleteUser() {
     Swal.fire("Delete Successfull...");
   }
@@ -14,76 +34,144 @@ const Dashboard = () => {
   function getuser() {
     fetch("http://15.206.27.173:3002/api/all-data").then((result) => {
       result.json().then((res) => {
-        console.warn("result", res);
+        // console.warn("result", res);
         setData(res.response);
       });
     });
   }
-  console.warn(data);
+  // console.warn(data);
 
   function deletedataemp(id) {
     fetch(`http://15.206.27.173:3002/api/delete-data/${id}`, {
       method: "DELETE",
     }).then((result) => {
       result.json().then((res) => {
-        console.warn(res);
+        // console.warn(res);
         deleteUser();
         getuser();
       });
     });
   }
 
-  const [First_name, setfname] = useState("");
-  const [Last_name, setlname] = useState("");
-  const [Email, setemail] = useState("");
-  const [Phone_no, setphone] = useState("");
-  const [Dob, setdob] = useState("");
-  const [Gender, setgender] = useState("");
+  // function saveUser() {
+  //   // insertData()
+  //   if(!user.First_name || !user.Last_name || !user.Email || !user.Phone_no || !user.Dob || !user.Gender){
 
-  function saveUser() {
-    insertData();
-    if(!First_name || !Last_name || !Email || !Phone_no || !Dob || !Gender){
-      Swal.fire("Enter all the fields...");
-    }
-    else{
-      Swal.fire("Added Successfull...");
-    }
-  }
+  //     insertData()
 
-  async function insertData() {
-    let data = { First_name, Last_name, Email, Phone_no, Dob, Gender };
-    // console.warn(data);
+  //     Swal.fire("Enter all fields..");
+  //   }
+  //   else{
+  //     Swal.fire("Added Successfull..");
 
-    let output = await fetch("http://15.206.27.173:3002/api/create_new", {
+  //   }
+
+  // }
+
+  const insertData = async () => {
+    let { First_name, Last_name, Email, Phone_no, Dob, Gender } = user;
+    // console.warn(user);
+
+    let output = await fetch("http://15.206.27.173:3002/api/create-new", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        First_name,
+        Last_name,
+        Email,
+        Phone_no,
+        Dob,
+        Gender,
+      }),
+      // body : JSON.stringify(user),
     });
     const d = await output.json();
 
-    console.log("output", d);
+    if (
+      !user.First_name ||
+      !user.Last_name ||
+      !user.Email ||
+      !user.Phone_no ||
+      !user.Dob ||
+      !user.Gender
+    ) {
+      Swal.fire("Enter all fields..");
+    } else {
+      Swal.fire("Added Successfull..");
+    }
+    // console.log("output", d);
+    getuser();
+  };
+
+  const handleEvent = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    // console.log("state",user)
+    if (
+      !user.First_name ||
+      !user.Last_name ||
+      !user.Email ||
+      !user.Phone_no ||
+      !user.Dob ||
+      !user.Gender
+    ) {
+      setBtn(true);
+      setBorderColor("1px solid");
+    } else {
+      setBtn(false);
+    }
+  };
+
+
+  const handleEvent2 = (e) => {
+    setUpdateUser({ ...updateUser, [e.target.name]: e.target.value });
+    // console.log("state",user)
+    console.log(updateUser)
+  };
+  // update phone no
+  const updateBtnFunc = (items, i) => {
+    console.log("updateBtnFunc", items);
+    setUpdateUser(items)
+  };
+
+  // update user func
+  const updateUserFunc=async(Id)=>{
+    let Phone_no=updateUser.Phone_no
+    console.log("eeeee",Phone_no)
+    let output = await fetch(`http://15.206.27.173:3002/api/update-data/${Id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        Phone_no
+      }),
+      // body : JSON.stringify(user),
+    });
+    const res=await output.json()
+    console.log("updated response",res)
     getuser();
   }
   return (
     <div>
-      <button 
+      <button
         type="button"
         className="btn btn-outline-dark my-5 ml-auto m-4 "
         data-toggle="modal"
-        data-target="#exampleModalCenter"
+        data-target="#addUser"
       >
         Add User
       </button>
       {/* Model */}
       <div
         className="modal fade"
-        id="exampleModalCenter"
+        id="addUser"
         tabIndex="-1"
         role="dialog"
-        aria-labelledby="exampleModalCenterTitle"
+        aria-labelledby="addUserTitle"
         aria-hidden="true"
       >
         <div className="modal-dialog modal-dialog-centered" role="document">
@@ -102,68 +190,78 @@ const Dashboard = () => {
               </button>
             </div>
             <div className="modal-body">
-              <form>
+              <form className="form-control" role="form" id="newModelForm">
                 <div className="form-group">
-                  <label>First Name</label>
+                  <label htmlFor="First_name">First Name</label>
                   <input
-                  required
-                    value={First_name}
-                    onChange={(e) => setfname(e.target.value)}
+                    style={{ border: borderColor }}
+                    required={true}
+                    value={user.First_name}
+                    name="First_name"
+                    onChange={handleEvent}
                     placeholder="mohit"
                     type="text"
-                    className="form-control"
-                    id="exampleInputEmail1"
+                    className="form-control required error"
+                    id="First_name"
                     aria-describedby="EmailHelp"
                   />
                 </div>
 
                 <div className="form-group">
-                  <label>Last Name</label>
+                  <label htmlFor="Last_name">Last Name</label>
                   <input
-                    value={Last_name}
+                    value={user.Last_name}
+                    style={{ border: borderColor }}
+                    name="Last_name"
                     required
-                    onChange={(e) => setlname(e.target.value)}
+                    onChange={handleEvent}
                     placeholder="sahu"
                     type="text"
                     className="form-control"
-                    id="exampleInputPassword1"
+                    id="Last_name"
                   />
                 </div>
                 <div className="form-group">
-                  <label>Email</label>
+                  <label htmlFor="Email">Email</label>
                   <input
-                    value={Email}
-                    onChange={(e) => setemail(e.target.value)}
+                    value={user.Email}
+                    style={{ border: borderColor }}
+                    onChange={handleEvent}
                     placeholder="xyz@gmail.com"
                     type="text"
+                    name="Email"
                     className="form-control"
                     required
-                    id="exampleInputPassword1"
+                    id="Email"
                   />
                 </div>
 
                 <div className="form-group">
-                  <label>Phone no.</label>
+                  <label htmlFor="Phone_no">Phone no.</label>
                   <input
-                    value={Phone_no}
-                    onChange={(e) => setphone(e.target.value)}
+                    value={user.Phone_no}
+                    onChange={handleEvent}
+                    style={{ border: borderColor }}
                     placeholder="1234567898"
+                    name="Phone_no"
                     type="text"
                     required
                     className="form-control"
-                    id="exampleInputPassword1"
+                    id="Phone_no"
                   />
                 </div>
                 <div className="form-group">
-                  <label>DOB</label>
+                  <label htmlFor="DOB">DOB</label>
                   <input
-                    value={Dob}
-                    onChange={(e) => setdob(e.target.value)}
+                    value={user.Dob}
+                    onChange={handleEvent}
+                    style={{ border: borderColor }}
                     placeholder="YYYY-MM-DD"
                     type="date"
                     required={true}
+                    name="Dob"
                     className="form-control"
-                    id="exampleInputPassword1"
+                    id="DOB"
                   />
                 </div>
                 <div className="input-group mt-2">
@@ -171,10 +269,12 @@ const Dashboard = () => {
                     <label className="input-group-text">Gender</label>
                   </div>
                   <select
-                    value={Gender}
-                    onChange={(e) => setgender(e.target.value)}
+                    value={user.Gender}
+                    onChange={handleEvent}
+                    style={{ border: borderColor }}
                     className="custom-select"
-                    id="inputGroupSelect01"
+                    id="Gender"
+                    name="Gender"
                   >
                     <option>Choose...</option>
                     <option>Male</option>
@@ -185,28 +285,29 @@ const Dashboard = () => {
               </form>
             </div>
             <div className="modal-footer">
+            <button
+                type="button"
+                className="btn btn-primary"
+                onClick={insertData}
+                data-dismiss="modal"
+                disabled={btn}
+              >
+                Save
+              </button>
               <button
                 type="button"
                 className="btn btn-secondary"
                 data-dismiss="modal"
               >
-                Cancle
+                Cancel
               </button>
 
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={saveUser}
-                data-dismiss="modal"
-              >
-                Save
-              </button>
+              
             </div>
           </div>
         </div>
       </div>
       <div className="container">
-        {" "}
         <table className="table table-striped">
           <thead>
             <tr>
@@ -217,42 +318,159 @@ const Dashboard = () => {
               {/* <th scope="col">DOB</th> */}
               <th scope="col">Phone no.</th>
               <th scope="col">Email</th>
-            
-              
             </tr>
           </thead>
           {data.map((items, i) => (
-            <tbody key={i}>
-              <tr>
-                <th scope="row">{items.Id}</th>
-                <td>{items.First_name}</td>
-                <td>{items.Last_name}</td>
-                <td>{items.Gender}</td>
-                {/* <td>{items.DOB}</td> */}
-                <td>{items.Phone_no}</td>
-                <td>{items.Email}</td>
-                <td>
-                  {/* <button
-                    data-toggle="modal"
-                    data-target="#exampleModalCenter"
-                    className="btn btn-dark"
-                  >
-                    Update
-                  </button> */}
-                </td>
-                <td>
-                  <button 
-                    className="btn btn-danger mx-2"
-                    onClick={() => deletedataemp(items.Id)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            </tbody>
+            <>
+              <tbody key={i}>
+                <tr>
+                  <th scope="row">{items.Id}</th>
+                  <td>{items.First_name}</td>
+                  <td>{items.Last_name}</td>
+                  <td>{items.Gender}</td>
+                  {/* <td>{items.DOB}</td> */}
+                  <td>{items.Phone_no}</td>
+                  <td>{items.Email}</td>
+                  <td>
+                    <button
+                      data-toggle="modal"
+                      data-target="#updateUser"
+                      className="btn btn-dark"
+                      onClick={() => updateBtnFunc(items, i)}
+                    >
+                      Update
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger mx-2"
+                      onClick={() => deletedataemp(items.Id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </>
           ))}
         </table>
       </div>
+      {/* update */}
+      {
+        [updateUser].map((elem,i)=>{
+          return <div
+          key={i}
+          className="modal fade"
+          id="updateUser"
+          tabIndex="-1"
+          aria-labelledby="updateUserLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="updateUserLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <form
+                  key={i}
+                  className="form-control"
+                  role="form"
+                  id="newModelForm"
+                >
+                  <div className="form-group">
+                    <label htmlFor="First_name">First Name</label>
+                    <input
+                      style={{ border: borderColor }}
+                      required={true}
+                      value={elem.First_name}
+                      name="First_name"
+                      onChange={handleEvent}
+                      placeholder="mohit"
+                      type="text"
+                      className="form-control required error"
+                      id="First_name"
+                      disabled={true}
+                      aria-describedby="EmailHelp"
+                    />
+                  </div>
+  
+                  <div className="form-group">
+                    <label htmlFor="Last_name">Last Name</label>
+                    <input
+                      value={elem.Last_name}
+                      style={{ border: borderColor }}
+                      name="Last_name"
+                      required
+                      onChange={handleEvent}
+                      placeholder="sahu"
+                      type="text"
+                      disabled={true}
+                      className="form-control"
+                      id="Last_name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="Email">Email</label>
+                    <input
+                      value={elem.Email}
+                      style={{ border: borderColor }}
+                      onChange={handleEvent}
+                      placeholder="xyz@gmail.com"
+                      type="text"
+                      name="Email"
+                      className="form-control"
+                      required
+                      disabled={true}
+                      id="Email"
+                    />
+                  </div>
+  
+                  <div className="form-group">
+                    <label htmlFor="Phone_no">Phone no.</label>
+                    <input
+                      value={elem.Phone_no}
+                      onChange={handleEvent2}
+                      style={{ border: borderColor }}
+                      placeholder="1234567898"
+                      name="Phone_no"
+                      type="text"
+                      required
+                      className="form-control"
+                      id="Phone_no"
+                    />
+                  </div>
+              
+                </form>
+              </div>
+              <div className="modal-footer">
+              <button onClick={()=>updateUserFunc(elem.Id)} type="button" className="btn btn-primary">
+                  Save changes
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+        })
+      }
     </div>
   );
 };
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,17 +10,17 @@ export default function Navbar() {
       navigate("/")
     }
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">{a.user.name}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand" >{a.user.name}</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           
 
-        <button onClick={logoutBtn} class="btn btn-outline-success " type="submit">Logout</button>
+        <button onClick={logoutBtn} className="btn btn-outline-success " type="submit">Logout</button>
         </ul>
        
       </div>
